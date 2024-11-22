cd198f3006ac1f021bbc7d4d797433852236754d
guthox <gutho.san@gmail.com>
2024-11-22 15:24:44 -0300

    Melhorias menu de jogos

--- lib/MenuJogos.dart
+++ lib/MenuJogos.dart
@@ -1,10 +1,11 @@
 import "package:flutter/material.dart";
 import "package:semeador/utils/CardResponsivo.dart";
+import "package:semeador/utils/CoresCustomizadas.dart";
 import "package:semeador/utils/Navegacao.dart";
 import "package:semeador/utils/NomesPath.dart";
 import "package:semeador/utils/TextoCustomizado.dart";
 
-class MenuJogos extends StatelessWidget{
+class MenuJogos extends StatelessWidget {
 
   final List<Widget> cartoesJogos = [];
   @override
@@ -13,6 +14,7 @@ class MenuJogos extends StatelessWidget{
     cartoesJogos.add(CardResponsivo(imagemPath: NomesPath.escondido, operacaoBotao: FuncaoBotao.telaJogoMemoria, texto: "Memória"));
     cartoesJogos.add(CardResponsivo(imagemPath: NomesPath.cachorro, operacaoBotao: FuncaoBotao.telaJogoQuebraCabeca, texto: "Cabeça"));
     cartoesJogos.add(CardResponsivo(imagemPath: NomesPath.tigre, operacaoBotao: FuncaoBotao.telaJogoAdvinha, texto: "Adivinha"));
+    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -20,20 +22,30 @@ class MenuJogos extends StatelessWidget{
             child: TextoCustomizado(texto: "Escolha um jogo", tamanhoFonte: 48.0),
           ),
         ),
-        body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-          itemBuilder: 
-            (context, index){
-              return Padding(
-                padding: EdgeInsets.all(8.0),
-                child: cartoesJogos[index],
-              );
-            },
-            itemCount: 4, 
+        body: Center( 
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, 
+            children: [
+              GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
+                ),
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: cartoesJogos[index],
+                  );
+                },
+                itemCount: 4,
+                shrinkWrap: true, 
+                physics: NeverScrollableScrollPhysics(),
+              ),
+            ],
           ),
-        )
+        ),
+      ),
     );
   }
-
-  
 }
--- lib/utils/CardResponsivo.dart
+++ lib/utils/CardResponsivo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import "package:semeador/utils/Navegacao.dart";
+import 'package:semeador/utils/Responsividade.dart';
 
 class CardResponsivo extends StatelessWidget{
   final String imagemPath;
@@ -16,6 +17,8 @@ class CardResponsivo extends StatelessWidget{
         operacaoBotao != FuncaoBotao.nada ? Navegacao.mudarTela(operacaoBotao, context) : {};
       }, 
       child: Container(
+        width: Responsividade.ehCelular(context) ? 200 : Responsividade.ehTablet(context) ? 400 : 500,
+        height: Responsividade.ehCelular(context) ? 200 : Responsividade.ehTablet(context) ? 400 : 500,
         color: cor,
         child: Column(
           children: [
